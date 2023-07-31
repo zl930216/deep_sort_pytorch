ef568748a2d82f0ba76764064d49ad94e4d00d7d
zl930216 <532165801@qq.com>
2023-07-31 13:41:17 +0800

    numpy float32

--- deep_sort/sort/detection.py
+++ deep_sort/sort/detection.py
@@ -29,7 +29,7 @@ class Detection(object):
     def __init__(
         self, tlwh: np.ndarray, confidence: float, feature: np.ndarray
     ) -> None:
-        self.tlwh = np.asarray(tlwh, dtype=np.float)
+        self.tlwh = np.asarray(tlwh, dtype=np.float32)
         self.confidence = float(confidence)
         self.feature = np.asarray(feature, dtype=np.float32)
 
